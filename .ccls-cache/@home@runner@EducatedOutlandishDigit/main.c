dbac3c91fe5f6b2ac6edb873f585cad5364f0179
5255517 <>
2023-02-14 11:33:43 +0000

    question 3

--- .ccls-cache/@home@runner@EducatedOutlandishDigit/main.c
+++ .ccls-cache/@home@runner@EducatedOutlandishDigit/main.c
@@ -4,57 +4,27 @@ Author: Edgar Tulodziecki
 Date: 14/02/2023
 Version: 1 */
 #include <stdio.h>
+#include <stdlib.h>
 
-int a;
-int b;
-int c;
-int d;
-int max1;
-int max2;
-int max3;
-int max;
-int min1;
-int min2;
-int min3;
-int min;
+int guess;
+char ans[6];
 
 int main(void) {
-  printf("Enter Number: \n");
-  scanf("%d", &a);
-  printf("Enter Number: \n");
-  scanf("%d", &b);
-  printf("Enter Number: \n");
-  scanf("%d", &c);
-  printf("Enter Number: \n");
-  scanf("%d", &d);
-
-  if(a>b){
-    max1 = a;
-    min1 = b;
+  printf("Guess Number: \n");
+  scanf("%d", &guess);
+  srand(time(NULL));
+  int random = rand() % 101;
+  if(guess == random){
+    printf("Congrats you got it!");
+      return 0;
+    }
+  if(guess > random){
+    printf("Too High");
   }
   else {
-    max1 = b;
-    min1 = a;
-  }
-  if(c>d){
-    max2 = c;
-    min2 = d;
-  }
-  else{
-    max2 = d;
-    min2 = c;
-  }
-  if(max1>max2){
-    max = max1;
-  }
-  else max = max2;
-  if(min1<min2){
-    min = min1;
-  }
-  else min = min2;
-
-  
-  printf("Max: %d Min: %d", max, min);
+    printf("Too High");
+    }
+  printf("Max: %d Min: %d", max);
 
   
   return 0;
--- main.c
+++ main.c
@@ -4,58 +4,24 @@ Author: Edgar Tulodziecki
 Date: 14/02/2023
 Version: 1 */
 #include <stdio.h>
+#include <stdlib.h>
 
-int a;
-int b;
-int c;
-int d;
-int max1;
-int max2;
-int max3;
-int max;
-int min1;
-int min2;
-int min3;
-int min;
-
+int guess;
 int main(void) {
-  printf("Enter Number: \n");
-  scanf("%d", &a);
-  printf("Enter Number: \n");
-  scanf("%d", &b);
-  printf("Enter Number: \n");
-  scanf("%d", &c);
-  printf("Enter Number: \n");
-  scanf("%d", &d);
-
-  if(a>b){
-    max1 = a;
-    min1 = b;
+  srand(time(NULL));
+  int random = rand() % 101;
+  while(guess!= random){
+    printf("Guess Number: ");
+    scanf("%d", &guess);
+    if(guess > random){
+      printf("Too High \n");
+    }
+    else if(guess < random) {
+      printf("Too Low \n");
+    }
   }
-  else {
-    max1 = b;
-    min1 = a;
-  }
-  if(c>d){
-    max2 = c;
-    min2 = d;
-  }
-  else{
-    max2 = d;
-    min2 = c;
-  }
-  if(max1>max2){
-    max = max1;
-  }
-  else max = max2;
-  if(min1<min2){
-    min = min1;
-  }
-  else min = min2;
-
-  
-  printf("Max: %d Min: %d", max, min);
-
-  
+  if(guess == random){
+    printf("Congrats you got it!");
+    }
   return 0;
 }
